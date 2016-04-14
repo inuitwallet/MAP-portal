4c29a204c1d1a06210a263a5b1a9edf1138bdf78
sammoth <sam.griffiths@wazoku.com>
2016-04-14 08:23:04 +0100

    try hardcoding some email settings

--- MAP/settings.py
+++ MAP/settings.py
@@ -122,11 +122,11 @@ AUTHENTICATION_BACKENDS = (
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = os.environ.get('EMAIL_HOST', None)
-EMAIL_PORT = os.environ.get('EMAIL_PORT', None)
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.google.com')
+EMAIL_PORT = os.environ.get('EMAIL_PORT', 587)
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', None)
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', None)
-EMAIL_USE_TLS = True if os.environ.get('EMAIL_USE_TLS', None) == 1 else False
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', True)
 
 USERENA_SIGNIN_REDIRECT_URL = '/%(username)s/'
 LOGIN_URL = '/signin/'
