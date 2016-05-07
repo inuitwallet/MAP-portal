562a55836638f3102787d447db4f0460a04ddf52
sammoth <sam.griffiths@wazoku.com>
2016-05-07 21:33:13 +0100

    add on screen keyboard

--- map/models.py
+++ map/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.utils.translation import ugettext as _
 from userena.models import UserenaLanguageBaseProfile
 from django.db import models
@@ -22,6 +23,7 @@ class Profile(UserenaLanguageBaseProfile):
     # mouse_vanish = models.BooleanField(default=False)
     # screen_reader = models.BooleanField(default=False)
     show_sounds = models.BooleanField(default=False)
-    focus_border = models.IntegerField(default=0)
+    focus_border = models.IntegerField(default=1)
     magnifier = models.BooleanField(default=False)
+    on_screen_keyboard = models.BooleanField(default=False)
 
